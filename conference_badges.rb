f02eb13a3b1e59dd98b99af562862b61eec34baa
Rene Avila <reneavila1993@gmail.com>
2020-05-14 19:49:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(list)
+  list.each_with_index do |attendee, index|
+    
+  end
+end
